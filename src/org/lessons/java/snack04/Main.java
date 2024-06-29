0009d68ff47332b87a5cbefbcbe6ef9e8afae55e
michelegelmini <164894307+michelegelmini@users.noreply.github.com>
2024-06-29 09:33:55 +0200

    add: snack-04

---
+++ src/org/lessons/java/snack04/Main.java
@@ -0,0 +1,29 @@
+package org.lessons.java.snack04;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		lista.add("Mario");
+		lista.add("Concetta");
+		lista.add("Luigi");
+		lista.add("Genoveffa");
+		lista.add("Claudia");
+		lista.add("Aldo");
+		lista.add("Giovanni");
+		lista.add("Giacomo");
+		lista.add("Luciana");
+		
+		System.out.println(lista);
+
+		Collections.sort(lista);
+
+
+		System.out.println(lista);
+	}
+
+}
