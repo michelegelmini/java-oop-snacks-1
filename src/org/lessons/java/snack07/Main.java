435549398706e2bb7b5ed701faee3d88981d0a3f
michelegelmini <164894307+michelegelmini@users.noreply.github.com>
2024-08-14 14:52:19 +0200

    add: searching_function

--- src/org/lessons/java/snack07/Main.java
+++ src/org/lessons/java/snack07/Main.java
@@ -1,11 +1,15 @@
 package org.lessons.java.snack07;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-
+	
+	static Scanner scanner = new Scanner(System.in);
+	static ArrayList<String> guests = new ArrayList<String>();
+	
 	public static void main(String[] args) {
 		
-		ArrayList<String> guests = new ArrayList<String>();
+		String inputName;
 		
 		guests.add("Marietto");
 		guests.add("Giovanni");
@@ -16,7 +20,22 @@ public class Main {
 		guests.add("Antonio");
 		guests.add("Benedetta");
 		
+		
+		System.out.println("Chi vuoi cercare?");
+		inputName = scanner.nextLine();
+		
+		
+		System.out.println(findElement(inputName));
 
+	}
+	
+	public static int findElement(String nameToSearch) {
+		if (guests.contains(nameToSearch)) {
+			return 1;
+		} else {		
+		return 0;
+		}
+		
 	}
 
 }
